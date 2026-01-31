6b6b3033956749de487718d5815ac61be0c2fa10
ZYNTEAI <pablonavarrorui@gmail.com>
2026-01-31 02:22:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -336,7 +336,7 @@ except:
     genai.configure(api_key="TU_CLAVE_AQUI")
 
 # Usamos el nombre sin el prefijo "models/" para que la librería lo gestione
-MODELO_USADO = "models/gemini-flash-latest"
+MODELO_USADO = "gemini-1.5-pro"
 
 # ==============================================================================
 # ℹ️ PÁGINAS DE INFORMACIÓN 
@@ -817,6 +817,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
